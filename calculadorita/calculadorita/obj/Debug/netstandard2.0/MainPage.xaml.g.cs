3a50066f6134958f9627175911871a0fb8e97e5d
Eduardo9494 <1433341@senati.pe>
2023-09-21 15:53:17 -0500

    llamamos a los archivos de es para cambiar de diseño a los numeros y que sea mas realista nuestra calculadora

--- calculadorita/calculadorita/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ calculadorita/calculadorita/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace calculadorita {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label LblResult;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            LblResult = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "LblResult");
         }
     }
 }
